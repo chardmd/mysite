c5bc8e6f00393d2bac14b57fe2e9ac3f38cf6e14
Richard Dimalanta <chardmd.au@gmail.com>
2018-04-14 17:29:54 +1000

    initial commit.

---
+++ src/components/ContactButton/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './ContactButton.css'
+
+class ContactButton extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div className="ContactButton">
+        <div className="button-wrapper">
+          <button onBlur={() => this.setState({ show: false })}>
+            <a href="#">
+              <i className="icon fa fa-envelope" />
+              <span className="name">Get In Touch</span>
+            </a>
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ContactButton
---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Metadata from '../Metadata'
+import Social from '../Social'
+
+const Footer = () => (
+  <footer className="Footer">
+    <Social />
+    <Metadata />
+  </footer>
+)
+
+export default Footer
