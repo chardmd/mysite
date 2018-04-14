b9723e22af105b3b992abe9f98f2cd401e728828
Richard <richard.dimalanta@gmail.com>
2018-04-14 21:55:14 +1000

    updated the links.

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -9,7 +9,7 @@ const Navigation = () => (
         <Link to="/">Experience</Link>
       </li>
       <li>
-        <Link to="/experience">Toolbox</Link>
+        <Link to="/toolbox">Toolbox</Link>
       </li>
       <li>
         <Link to="/404">Contact</Link>
