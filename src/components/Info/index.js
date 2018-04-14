d97005d35f786f71c89b7b7dc5f97bb6bd32718b
Richard Dimalanta <chardmd.au@gmail.com>
2018-04-14 17:40:49 +1000

    Added info component.

---
+++ src/components/Info/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Info.css'
+
+const Info = () => (
+  <section className="Info">
+    <div />
+  </section>
+)
+
+export default Info
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import About from '../components/About'
+import Info from '../components/Info'
 
 import './index.css'
 
 const IndexPage = () => (
   <main>
     <About />
-    <About />
+    <Info />
   </main>
 )
 
