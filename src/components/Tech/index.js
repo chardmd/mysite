08b223fbe4612038e46437ad57453fd54f0474c9
Richard <richard.dimalanta@gmail.com>
2018-04-16 21:32:12 +1000

    improved the tech page by using stack grid.

--- src/components/Tech/index.js
+++ src/components/Tech/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import StackGrid from 'react-stack-grid'
 import './Tech.css'
 
 import angularIcon from '../../assets/angular-icon.svg'
@@ -33,41 +34,52 @@ import tomcat from '../../assets/tomcat.svg'
 import webstorm from '../../assets/webstorm.svg'
 import yeoman from '../../assets/yeoman.svg'
 
-const Tech = () => (
-  <section className="Tech">
-    <div className="grid">
-      <img src={angularIcon} alt="angular" />
-      <img src={aws} alt="aws" />
-      <img src={bootstrap} alt="bootstrap" />
-      <img src={bower} alt="bower" />
-      <img src={css3} alt="css3" />
-      <img src={eclipse} alt="eclipse" />
-      <img src={git} alt="git" />
-      <img src={githubIcon} alt="githubIcon" />
-      <img src={googleAnalytics} alt="googleAnalytics" />
-      <img src={grunt} alt="grunt" />
-      <img src={html5} alt="html5" />
-      <img src={java} alt="java" />
-      <img src={javascript} alt="javascript" />
-      <img src={jenkins} alt="jenkins" />
-      <img src={jira} alt="jira" />
-      <img src={jquery} alt="jquery" />
-      <img src={macOs} alt="macOs" />
-      <img src={mongodb} alt="mongodb" />
-      <img src={mysql} alt="mysql" />
-      <img src={nodejs} alt="nodejs" />
-      <img src={npm} alt="npm" />
-      <img src={pm2} alt="pm2" />
-      <img src={postgresql} alt="postgresql" />
-      <img src={redis} alt="redis" />
-      <img src={require} alt="require" />
-      <img src={sourceTree} alt="sourceTree" />
-      <img src={spring} alt="spring" />
-      <img src={terminal} alt="terminal" />
-      <img src={tomcat} alt="tomcat" />
-      <img src={webstorm} alt="webstorm" />
-    </div>
-  </section>
-)
+class Tech extends React.Component {
+  render() {
+    return (
+      <section className="Tech">
+        <div>
+          <StackGrid
+            columnWidth={140}
+            className="grid"
+            gutterWidth={10}
+            gutterHeight={10}
+          >
+            <img src={angularIcon} alt="angular" />
+            <img src={aws} alt="aws" />
+            <img src={bootstrap} alt="bootstrap" />
+            <img src={bower} alt="bower" />
+            <img src={css3} alt="css3" />
+            <img src={eclipse} alt="eclipse" />
+            <img src={git} alt="git" />
+            <img src={githubIcon} alt="githubIcon" />
+            <img src={googleAnalytics} alt="googleAnalytics" />
+            <img src={grunt} alt="grunt" />
+            <img src={html5} alt="html5" />
+            <img src={java} alt="java" />
+            <img src={javascript} alt="javascript" />
+            <img src={jenkins} alt="jenkins" />
+            <img src={jira} alt="jira" />
+            <img src={jquery} alt="jquery" />
+            <img src={macOs} alt="macOs" />
+            <img src={mongodb} alt="mongodb" />
+            <img src={mysql} alt="mysql" />
+            <img src={nodejs} alt="nodejs" />
+            <img src={npm} alt="npm" />
+            <img src={pm2} alt="pm2" />
+            <img src={postgresql} alt="postgresql" />
+            <img src={redis} alt="redis" />
+            <img src={require} alt="require" />
+            <img src={sourceTree} alt="sourceTree" />
+            <img src={spring} alt="spring" />
+            <img src={terminal} alt="terminal" />
+            <img src={tomcat} alt="tomcat" />
+            <img src={webstorm} alt="webstorm" />
+          </StackGrid>
+        </div>
+      </section>
+    )
+  }
+}
 
 export default Tech
