70b49f03c9df489dccbf8d1104f6f4fc90881066
Richard <richard.dimalanta@gmail.com>
2018-04-16 21:01:07 +1000

    updated metadata link.

--- src/components/Metadata/index.js
+++ src/components/Metadata/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import Link from 'gatsby-link'
 import './Metadata.css'
 
 const Metadata = () => (
   <div className="Metadata">
     <span>
       Handcrafted by{' '}
-      <a href="#" target="_blank">
-        Richard 😎{' '}
-      </a>
+      <Link exact to="/">
+        Richard <span>😎</span>
+      </Link>
       <br />
       Currently based in Sydney, Australia.
     </span>
