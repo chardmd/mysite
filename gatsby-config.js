0bc6673b63d213229e2ab2403259be34d0bd5d1e
Richard Dimalanta <chardmd.au@gmail.com>
2018-04-20 05:54:27 +0800

    add google analytics.

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,15 +7,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: 'YOUR_GOOGLE_ANALYTICS_TRACKING_ID',
+        trackingId: 'UA-117945705-1',
         // Puts tracking script in the head instead of the body
         head: false,
         // Setting this parameter is optional
         anonymize: true,
         // Setting this parameter is also optional
         respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ['/preview/**', '/do-not-track/me/too/'],
       },
     },
   ],
