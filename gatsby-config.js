cb7c1be74b6e847cdbf4d3ae720cdc4698d21ce8
Richard <richard.dimalanta@gmail.com>
2018-04-20 06:04:52 +0800

    updated ga settings.

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,10 +10,6 @@ module.exports = {
         trackingId: 'UA-117945705-1',
         // Puts tracking script in the head instead of the body
         head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
       },
     },
   ],
