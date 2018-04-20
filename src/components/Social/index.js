bea679e2011340432d9352e12ffa380a34447c35
Richard <richard.dimalanta@gmail.com>
2018-04-20 11:58:38 +0800

    updated text.

--- src/components/Social/index.js
+++ src/components/Social/index.js
@@ -9,7 +9,7 @@ const Social = () => (
   <div className="Social">
     <ul>
       <li>
-        <h3>You can also find him in </h3>
+        <h3>You can also find me in </h3>
       </li>
       <li>
         <OutboundLink href="https://github.com/chardmd" target="_blank">
