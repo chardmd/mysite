3f635216abd5c94cbb37306ff11caaae74b536dc
Richard <richard.dimalanta@gmail.com>
2018-04-29 15:11:24 +1000

    updated page not found animation

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,11 +3,11 @@ import giphy from '../assets/giphy.gif'
 
 const NotFoundPage = () => (
   <div className="pageNotFound">
-    <h1 className="animated rubberBand">Oopps.. Page Not Found</h1>
-    <p className="animated rubberBand">
-      You just hit a route that doesn&#39;t exist.
-    </p>
-    <img src={giphy} width="480" alt="excite" />
+    <div className="animated bounceIn">
+      <h1>Oopps.. Page Not Found</h1>
+      <p>You just hit a route that doesn&#39;t exist.</p>
+      <img src={giphy} width="480" alt="excite" />
+    </div>
   </div>
 )
 
