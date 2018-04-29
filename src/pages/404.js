fa40f5eb9c8c2c3a7473ce9f1984908190a36c9d
Richard Dimalanta <chardmd.au@gmail.com>
2018-04-29 14:48:25 +1000

    addded page not found.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import giphy from '../assets/giphy.gif'
 
 const NotFoundPage = () => (
-  <div style={{ width: '100%' }}>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  <div className="pageNotFound">
+    <h1 className="animated rubberBand">Oopps.. Page Not Found</h1>
+    <p className="animated rubberBand">
+      You just hit a route that doesn&#39;t exist.
+    </p>
+    <img src={giphy} width="480" alt="excite" />
   </div>
 )
 
