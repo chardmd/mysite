dc42e647ceebce95bd85d0fe546d3b506bccf463
Richard <richard.dimalanta@gmail.com>
2018-04-29 17:30:51 +1000

    changing title color on navigation change.

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,22 +4,56 @@ import Footer from '../Footer'
 
 import './About.css'
 
-const About = () => (
-  <div className="About">
-    <div>
-      <h1>Software Engineer</h1>
-      <h2>Richard Dimalanta</h2>
-      <Navigation />
-      <p>
-        Richard is skilled in building SaaS solutions using high performance web
-        technologies. He has a wide range of experience in the tech industry and
-        have worked across multiple countries such as Singapore, Philippines,
-        and currently Australia. He always aim to build services that makes
-        people's lives better.
-      </p>
-      <Footer />
-    </div>
-  </div>
-)
+class About extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      color: 'black',
+    }
+  }
 
+  componentDidMount() {
+    this.setTitleColor(this.props.location.pathname)
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setTitleColor(nextProps.location.pathname)
+  }
+
+  setTitleColor(pathname) {
+    if (pathname === '/toolbox') {
+      this.setState({
+        color: 'red',
+      })
+    } else if (pathname === '/contact') {
+      this.setState({
+        color: 'green',
+      })
+    } else {
+      this.setState({
+        color: 'black',
+      })
+    }
+  }
+
+  render() {
+    return (
+      <div className="About">
+        <div>
+          <h1>Software Engineer</h1>
+          <h2 className={this.state.color}>Richard Dimalanta</h2>
+          <Navigation />
+          <p>
+            Richard is skilled in building SaaS solutions using high performance
+            web technologies. He has a wide range of experience in the tech
+            industry and have worked across multiple countries such as
+            Singapore, Philippines, and currently Australia. He always aim to
+            build services that makes people's lives better.
+          </p>
+          <Footer />
+        </div>
+      </div>
+    )
+  }
+}
 export default About
