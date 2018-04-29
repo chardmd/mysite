95cc7fbbe97d20ec3ac1b1a65b01f5f2849558fb
Richard <richard.dimalanta@gmail.com>
2018-04-29 10:41:07 +1000

    minor changes.

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -12,7 +12,7 @@ const About = () => (
       <Navigation />
       <p>
         Richard is skilled in building SaaS solutions using high performance web
-        technologies. He has wide range of experience in the tech industry and
+        technologies. He has a wide range of experience in the tech industry and
         have worked across multiple countries such as Singapore, Philippines,
         and currently Australia. He always aim to build services that makes
         people's lives better.
