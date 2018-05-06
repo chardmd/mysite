79c4a8a3a113d84c053619828b600d44b3f40fac
Richard Dimalanta <chardmd.au@gmail.com>
2018-05-06 15:54:57 +1000

    added animation on left content.

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -40,7 +40,7 @@ class About extends React.Component {
   render() {
     return (
       <div className="About">
-        <div>
+        <div className="animated bounceInLeft">
           <h1>Software Engineer</h1>
           <h2 className={this.state.color}>Richard Dimalanta</h2>
           <Navigation />
