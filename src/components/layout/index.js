0ec894b8e4f71efdb09efe688c77eb916165893d
Richard <richard.dimalanta@gmail.com>
2018-09-29 09:10:04 +1000

    use drift package

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
-import Drift from '../Drift'
+import Drift from 'react-driftjs'
 import { StaticQuery, graphql } from 'gatsby'
 import 'animate.css'
 
