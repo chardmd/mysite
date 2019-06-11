4be8d98db970caab90f70f25d5c5de598517703a
Richard <richard.dimalanta@gmail.com>
2019-06-11 21:54:55 +1000

    fixed issue on multiple script being added during page navigation

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,2 @@
-import React from 'react'
-import Layout from '../components/layout'
 import Contact from '../components/Contact'
-
-const ToolboxPage = props => (
-  <Layout location={props.location}>
-    <Contact />
-  </Layout>
-)
-
-export default ToolboxPage
+export default Contact
--- src/pages/toolbox.js
+++ src/pages/toolbox.js
@@ -1,11 +1,2 @@
-import React from 'react'
-import Layout from '../components/layout'
 import Toolbox from '../components/Toolbox'
-
-const ToolboxPage = props => (
-  <Layout location={props.location}>
-    <Toolbox />
-  </Layout>
-)
-
-export default ToolboxPage
+export default Toolbox
