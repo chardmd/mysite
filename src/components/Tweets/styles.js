ba0972072f452689bd59aa67c216591be1b8d0b7
Richard <richard.dimalanta@gmail.com>
2019-06-15 20:55:22 +1000

    make tweets functional component.

---
+++ src/components/Tweets/styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  height: 100%;
+  height: 100vh;
+  overflow-y: scroll;
+  border-left: 1px solid #e6ecf0;
+  @media (max-width: 800px) {
+    overflow: hidden;
+    height: 100%;
+  }
+`
+
+export const Content = styled.div`
+  z-index: 99999;
+  width: 100%;
+`
