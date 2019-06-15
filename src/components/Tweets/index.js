9f28fcb93d56d1107a731a42dfeea035293ab771
Richard Dimalanta <chardmd.au@gmail.com>
2019-06-15 20:55:22 +1000

    make tweets functional component.

--- src/components/Tweets/index.js
+++ src/components/Tweets/index.js
@@ -1,52 +1,30 @@
 /* global twttr */
-import React, { Fragment } from 'react'
-import ReactDOM from 'react-dom'
-import './Tweets.css'
+import React, { useEffect, useRef } from 'react'
+import { Container, Content } from './styles'
 
-class Tweets extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = { initialized: false }
-  }
+const Tweets = () => {
+  const containerRef = useRef(null)
 
-  componentDidMount() {
-    if (this.state.initialized) {
-      return
-    }
-
+  useEffect(() => {
     if (typeof twttr === 'undefined') {
-      const twittertimeline = ReactDOM.findDOMNode(this)
       const twitterscript = document.createElement('script')
       twitterscript.src = 'https://platform.twitter.com/widgets.js'
       twitterscript.async = true
       twitterscript.id = 'twitter-wjs'
-      twittertimeline.parentNode.appendChild(twitterscript)
+      containerRef.current.parentNode.appendChild(twitterscript)
     } else {
-      twttr.widgets.load()
+      twttr.widgets.load(document.getElementById(containerRef.current))
     }
+  }, [])
 
-    this.initialized()
-  }
-
-  initialized() {
-    this.setState({ initialized: true })
-  }
-
-  render() {
-    return (
-      <Fragment>
-        <div className="Tweets">
-          <div className="content">
-            <a
-              className="twitter-timeline"
-              href="https://twitter.com/iamchardmd"
-            >
-              &nbsp;
-            </a>
-          </div>
-        </div>
-      </Fragment>
-    )
-  }
+  return (
+    <Container ref={containerRef}>
+      <Content>
+        <a className="twitter-timeline" href="https://twitter.com/iamchardmd">
+          &nbsp;
+        </a>
+      </Content>
+    </Container>
+  )
 }
 export default Tweets
