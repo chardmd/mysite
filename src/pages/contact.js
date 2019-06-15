41a7e51efce64722fddb7c0bd8101ba65499414f
Richard <richard.dimalanta@gmail.com>
2019-06-15 21:40:27 +1000

    reverted back to v2 layout

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,2 +1,12 @@
+import React from 'react'
 import Contact from '../components/Contact'
-export default Contact
+import Layout from '../components/layout'
+
+const ContactPage = ({ location }) => {
+  return (
+    <Layout location={location}>
+      <Contact />
+    </Layout>
+  )
+}
+export default ContactPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,12 @@
+import React from 'react'
 import Tweets from '../components/Tweets'
-export default Tweets
+import Layout from '../components/layout'
+
+const HomePage = ({ location }) => {
+  return (
+    <Layout location={location}>
+      <Tweets />
+    </Layout>
+  )
+}
+export default HomePage
