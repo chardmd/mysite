687f1d6e8b1556ea3ba0f91a824e4eeab986167a
Richard Dimalanta <chardmd.au@gmail.com>
2019-07-02 20:27:19 +1000

    make pageNotFound a component

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,2 @@
-import React from 'react'
-import giphy from '../assets/giphy.gif'
-
-const NotFoundPage = () => (
-  <div className="pageNotFound">
-    <div className="animated bounceIn">
-      <h1>Oopps.. Page Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist.</p>
-      <img src={giphy} width="480" alt="excite" />
-    </div>
-  </div>
-)
-
-export default NotFoundPage
+import NotFound from '../components/NotFound'
+export default NotFound
