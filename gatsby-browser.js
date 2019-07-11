611999bcb1df59a9f873f433e18d29a30c9bee92
Richard Dimalanta <chardmd.au@gmail.com>
2019-07-11 23:03:15 +1000

    revert to the old twitter timeline solution

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,27 +3,3 @@
  *
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
-
-let injectedTwitterScript = false
-
-const injectTwitterScript = () => {
-  const twitterscript = document.createElement('script')
-  twitterscript.src = 'https://platform.twitter.com/widgets.js'
-  twitterscript.async = true
-  twitterscript.id = 'twitter-wjs'
-  document.getElementsByTagName(`head`)[0].appendChild(twitterscript)
-}
-
-exports.onClientEntry = () => {
-  if (!injectedTwitterScript) {
-    injectTwitterScript()
-    injectedTwitterScript = true
-  }
-  if (
-    typeof twttr !== 'undefined' &&
-    window.twttr.widgets &&
-    typeof window.twttr.widgets.load === 'function'
-  ) {
-    window.twttr.widgets.load()
-  }
-}
