bad9d1b5f0d43b8b7df1cd4678ae927932ce3341
Richard <richard.dimalanta@gmail.com>
2019-07-11 22:31:46 +1000

    added loading animation for tweets

---
+++ src/components/Tweets/keyFrames.js
@@ -0,0 +1,95 @@
+import { keyframes } from 'styled-components'
+
+export const load = keyframes`
+	0% {
+		top: 9.5rem;
+	}
+	70% {
+		top: 2.125rem;
+	}
+	90% {
+		top: 0;
+	}
+	95% {
+		top: 0;
+	}
+	100% {
+		top: 9.5rem;
+	}
+`
+
+export const liquid1 = keyframes`
+	0% {
+		height: 0;
+		opacity: 0;
+		top: -1.5rem;
+	}
+	22% {
+		height: 0.28125rem;
+		top: 0.375rem;
+		opacity: 1;
+	}
+	25% {
+		top: -0.25rem;
+	}
+	35% {
+		height: 2.125rem;
+		top: -1.5rem;
+	}
+	55% {
+		height: 0.28125rem;
+		top: -0.125rem;
+	}
+	60% {
+		height: 1.61875rem;
+		opacity: 1;
+		top: -0.275rem;
+	}
+	96% {
+		height: 1.84375rem;
+		opacity: 0;
+		top: 1.5rem;
+	}
+	100% {
+		height: 0;
+		opacity: 0;
+	}
+`
+
+export const liquid2 = keyframes`
+	0% {
+		height: 0;
+		opacity: 0;
+		top: -1.5rem;
+	}
+	17.5% {
+		height: 0.28125rem;
+		top: 0.2rem;
+		opacity: 1;
+	}
+	20% {
+		top: -0.25rem;
+	}
+	25% {
+		height: 3.40625rem;
+		top: -1.625rem;
+	}
+	45% {
+		height: 0.28125rem;
+		top: -0.125rem;
+	}
+	60% {
+		height: 3.40625rem;
+		opacity: 1;
+		top: -1.5rem;
+	}
+	96% {
+		height: 1.84375rem;
+		opacity: 0;
+		top: 1.5rem;
+	}
+	100% {
+		height: 0;
+		opacity: 0;
+	}
+`
