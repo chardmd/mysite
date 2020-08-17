d9b5ce53a7ddf253d29fa1e691f6db1b7077a528
Richard Dimalanta <chardmd.au@gmail.com>
2020-08-17 18:16:03 +1000

    Update config.js

--- config.js
+++ config.js
@@ -9,13 +9,12 @@ module.exports = {
   googleAnalyticsId: "UA-175524956-1",
   email: "hello@chardmd.com",
   headline: "Richard M. Dimalanta",
-  about: `Richard is skilled in building web-apps (SaaS) using high cutting-edge technologies. 
-  He has been developing enterprise applications and distributed systems since 2011 and 
-  has gained a lot of experience working across global businesses. His skills in software development 
-  can bring significant value to the team and positive ROI to your organization. 
-  He has built and designed business software consisting of microservices, 
-  service-oriented, monolith, and serverless architecture. 
-  He will help you build software that increases the business' revenue / reduce costs.`,
+  about: `Richard is skilled in building web apps (SaaS) using cutting-edge technologies. 
+  He has been developing enterprise-grade applications and distributed systems since 2011. 
+  He capitalized his experience helping various companies across Australia and Southeast Asia in building production-ready software that works. 
+  His skills in software development can bring significant value to the team and positive ROI to your organization. 
+  For the past few years, Richard built a wide array of projects consisting of microservices, service-oriented, monolith, and serverless architecture. 
+  He will help you build software that will increase the business' revenue or reduce costs.`,
   twitter: "@iamchardmd",
   driftId: "nzed2gc9n3vk",
 };
