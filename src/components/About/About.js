c4f3c1d280b78413a62b3b2d926dbcd82a9ec021
Richard Dimalanta <chardmd.au@gmail.com>
2020-09-09 20:23:30 +1000

    use three.js and apply boxes background

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,10 +1,24 @@
 import React, { useState, useEffect } from "react";
+import * as THREE from "three";
+import { Canvas } from "react-three-fiber";
+import { useSprings, a } from "react-spring/three";
 
 import Navigation from "../Navigation";
 import Social from "../Social";
 import Metadata from "../Metadata";
 import styles from "./About.module.scss";
 import useSiteMetadata from "../../hooks/use-site-metadata";
+
+const BOXES = 25;
+const COLORS = [
+  "#A2CCB6",
+  "#FCEEB5",
+  "#8bcdcd",
+  "#99f3bd",
+  "lightpink",
+  "lightblue",
+  "#fabea7",
+];
 
 const getTitleColorHeader = (pathname) => {
   let colorHeader = "";
@@ -34,6 +48,68 @@ const getHeadline = (pathname) => {
   return title;
 };
 
+const random = (i) => {
+  const r = Math.random();
+  return {
+    position: [100 - Math.random() * 200, 100 - Math.random() * 200, i * 1.5],
+    color: COLORS[Math.round(Math.random() * (COLORS.length - 1))],
+    scale: [1 + r * 14, 1 + r * 14, 1],
+    rotation: [0, 0, THREE.Math.degToRad(Math.round(Math.random()) * 45)],
+  };
+};
+
+const data = new Array(BOXES).fill().map(() => {
+  return {
+    color: COLORS[Math.round(Math.random() * (COLORS.length - 1))],
+    args: [0.1 + Math.random() * 9, 0.1 + Math.random() * 9, 10],
+  };
+});
+
+function Content() {
+  const [springs, set] = useSprings(BOXES, (i) => ({
+    from: random(i),
+    ...random(i),
+    config: { mass: 20, tension: 150, friction: 50 },
+  }));
+  useEffect(
+    () =>
+      void setInterval(
+        () => set((i) => ({ ...random(i), delay: i * 40 })),
+        3000
+      ),
+    []
+  );
+  return data.map((d, index) => (
+    <a.mesh key={index} {...springs[index]} castShadow receiveShadow>
+      <boxBufferGeometry attach="geometry" args={d.args} />
+      <a.meshStandardMaterial
+        attach="material"
+        color={springs[index].color}
+        roughness={0.75}
+        metalness={0.5}
+      />
+    </a.mesh>
+  ));
+}
+
+function Lights() {
+  return (
+    <group>
+      <pointLight intensity={0.3} />
+      <ambientLight intensity={2} />
+      <spotLight
+        castShadow
+        intensity={0.2}
+        angle={Math.PI / 7}
+        position={[150, 150, 250]}
+        penumbra={1}
+        shadow-mapSize-width={2048}
+        shadow-mapSize-height={2048}
+      />
+    </group>
+  );
+}
+
 const About = ({ location }) => {
   const siteMetadata = useSiteMetadata();
   const pathname = location.pathname;
@@ -48,6 +124,10 @@ const About = ({ location }) => {
 
   return (
     <div className={styles.container}>
+      <Canvas shadowMap camera={{ position: [0, 0, 100], fov: 100 }}>
+        <Lights />
+        <Content />
+      </Canvas>
       <div className={styles.wrapper}>
         <h1 className={`${styles.header} ${zoomIn}`}>{headline}</h1>
         <h2
