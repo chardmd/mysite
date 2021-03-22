406d2679482f7b13b6f8b4efbe934d6afeafd285
Richard Dimalanta <chardmd.au@gmail.com>
2021-03-22 19:54:13 +1100

    updated config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ module.exports = {
   He has been developing enterprise-grade applications and distributed systems since 2011. 
   He capitalized on his experience helping various companies across Australia and Southeast Asia in scaling and building "production-ready" software products. 
   His software development skills can bring significant value to the team and positive ROI to your organization. 
-  For the past years, Richard built a wide array of projects consisting of microservices, service-oriented, monolith, and serverless architecture. 
+  For the past years, Richard built a wide array of projects consisted of microservices, service-oriented, monolith, and serverless architecture. 
   He will help you build software that will increase the business' revenue or reduce costs.`,
   twitter: "@iamchardmd",
   driftId: "nzed2gc9n3vk",
