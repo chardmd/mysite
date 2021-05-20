c3f658536e6345d97d2bcbb5c3b7c08d1fe04909
Richard Dimalanta <chardmd.au@gmail.com>
2021-05-20 10:16:00 +1000

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ module.exports = {
   headline: "Richard M. Dimalanta",
   about: `Richard is skilled in building web apps (SaaS) using cutting-edge technologies. 
   He has been developing enterprise-grade applications and distributed systems since 2011. 
-  He capitalized on his experience helping various companies across Australia and Southeast Asia in scaling and building production-ready software products that "works". 
+  He capitalised on his technical experience helping various companies across Australia in building enterprise software solution that scales and keep up with the demand of the business. 
   His software development skills can bring significant value to the team and positive ROI to your organization. 
   For the past years, Richard built a wide array of projects consisted of microservices, service-oriented, monolith, and serverless architecture. 
   He will help you build software that will increase the business' revenue or reduce costs.`,
