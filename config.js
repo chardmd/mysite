b1fab0e2a56c4d9e5cd367413588081133a3b6cb
Richard Dimalanta <chardmd.au@gmail.com>
2021-05-28 15:00:25 +1000

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ module.exports = {
   email: "hello@chardmd.com",
   headline: "Richard M. Dimalanta",
   about: `Richard is a Seasoned Software Engineer developing enterprise-grade applications and distributed systems since 2011.
-  He capitalised on his technical experience helping various companies across Australia in building software solutions that are highly reliable, scalable, and secure.
+  He capitalised on his technical experience helping various companies across Australia 🇦🇺 in building software solutions that are highly reliable, scalable, and secure.
   His software development skills can bring significant value to the team and a positive ROI to your organization. In the past years, He built many projects consisting of micro-services, service-oriented, monolith, and serverless architecture.
   He can help you deliver enterprise applications that will increase the business' revenue or reduce costs.`,
   twitter: "@iamchardmd",
